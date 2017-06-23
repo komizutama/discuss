ebe9929bc397b03d36c9e07fa5694050c71d5dfd
Matthias Elliott <komizutama@gmail.com>
2017-06-23 17:37:03 -0400

    added new topic form and fixed the error messages.

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -1,11 +1,21 @@
 defmodule Discuss.TopicController do
   use Discuss.Web, :controller #In this line, phoenix reaches into web.ex folder notices that it's designed a controller... does lots of controller set up. fixes the init error
-  
+
   alias Discuss.Topic
 
-  def new(conn, params) do
+  def new(conn, _params) do
     changeset = Topic.changeset(%Topic{}, %{})
 
-    render conn, "new.html"
+    render conn, "new.html", changeset: changeset
+  end
+
+  def create(conn, %{"topic" => topic}) do
+    changeset = Topic.changeset(%Topic{}, topic)
+
+    case Repo.insert(changeset) do
+      {:ok, post} -> IO.inspect(post)
+      {:error, changeset} ->
+        render conn, "new.html", changeset: changeset
+    end
   end
 end
--- web/views/topic_view.ex
+++ web/views/topic_view.ex
@@ -1,3 +1,3 @@
 defmodule Discuss.TopicView do
-  use Discuss.Web, :controller
+  use Discuss.Web, :view
 end
